016cc7dbb31ae074dcb484ce697ed7bf1057f79d
pillowsofwind <0xrwxu@gmail.com>
2025-09-26 23:19:13 -0700

    update

--- javascripts/load_navbar.js
+++ javascripts/load_navbar.js
@@ -2,9 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const navbar = `
     <nav class="navbar">
         <div class="nav-content">
-            <a href="./index.html">home</a>
-            <a href="./publications.html">publications</a>
-            <a href="./cv.html">cv</a>
+            <a href="./index.html">home</a>     
             <a href="./misc.html">misc</a>
         </div>
     </nav>
